8287d330bfdfc2921eda1772fe21d58547eb5f6e
allanribeiro91 <akosr91@gmail.com>
2024-07-18 12:11:00 -0300

    Ajuste no main_contratos, para carregar as funções de scripts_public

--- projeto/contratos/main_contratos.py
+++ projeto/contratos/main_contratos.py
@@ -1,5 +1,13 @@
-from scripts_public.baixar_dados_srinfo import baixar_dados_srinfo
-from scripts_public.mover_arquivos import mover_arquivos_excel
+import os
+import sys
+
+#sys.path
+current_dir = os.path.dirname(os.path.abspath(__file__))
+scripts_public_path = os.path.abspath(os.path.join(current_dir, '..', '..', '..', 'ETL_SRInfo\scripts_public'))
+sys.path.append(scripts_public_path)
+
+from baixar_dados_srinfo import baixar_dados_srinfo
+from mover_arquivos import mover_arquivos_excel
 
 def main_contratos():
     link = 'https://srinfo.embrapii.org.br/projects/contracts/'

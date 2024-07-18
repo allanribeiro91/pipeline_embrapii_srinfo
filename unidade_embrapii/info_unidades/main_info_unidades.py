034754f71bba3caad2b518b37bc2225447ba4e5c
jhonatacarvalhoo <jhonata.silva@embrapii.org.br>
2024-07-18 15:11:57 -0300

    Informações das unidades.

---
+++ unidade_embrapii/info_unidades/main_info_unidades.py
@@ -0,0 +1,24 @@
+import os
+import sys
+
+#sys.path
+current_dir = os.path.dirname(os.path.abspath(__file__))
+scripts_public_path = os.path.abspath(os.path.join(current_dir, '..', '..', '..', 'etl_srinfo\scripts_public'))
+sys.path.append(scripts_public_path)
+
+from baixar_dados_srinfo import baixar_dados_srinfo
+from mover_arquivos import mover_arquivos_excel
+
+def main_info_unidades():
+    link = 'https://srinfo.embrapii.org.br/units/list/'
+    numero_arquivos = baixar_dados_srinfo(link)
+    print('Número de arquivos: ', numero_arquivos)
+    pasta_download = r'C:\Users\jhonata.silva\Downloads'
+    diretorio = current_dir
+    nome_arquivo = 'info_unidades'
+    mover_arquivos_excel(numero_arquivos, pasta_download, diretorio, nome_arquivo)
+    print('Finalizado!')
+
+
+if __name__ == "__main__":
+    main_info_unidades()

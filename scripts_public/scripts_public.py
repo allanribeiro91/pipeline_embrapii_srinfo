815c2849407dd40b450feb0b03d8512d60c925b1
allanribeiro91 <akosr91@gmail.com>
2024-07-19 11:58:42 -0300

    ajustes gerais.

--- scripts_public/scripts_public.py
+++ scripts_public/scripts_public.py
@@ -1,16 +1,19 @@
+import os
+from dotenv import load_dotenv
 from baixar_dados_srinfo import baixar_dados_srinfo
 from mover_arquivos import mover_arquivos_excel
 from append_arquivos import append_excel_files
 
-def baixar_e_juntar_arquivos(link, pasta_download, diretorio, nome_arquivo):
+# Carregar variáveis de ambiente do arquivo .env
+load_dotenv()
+
+def baixar_e_juntar_arquivos(link, diretorio, nome_arquivo):
+
+    pasta_download = os.getenv('PASTA_DOWNLOAD')
+
     numero_arquivos = baixar_dados_srinfo(link)
     mover_arquivos_excel(numero_arquivos, pasta_download, diretorio, nome_arquivo)
     append_excel_files(diretorio, nome_arquivo)
     print('Baixar e juntar arquivos: Finalizado!')
 
 
-
-link = 'https://srinfo.embrapii.org.br/projects/contracts/'
-pasta_download = r'C:\Users\allan.ribeiro\Downloads'
-diretorio = r'C:\Users\allan.ribeiro\OneDrive\Embrapii\ETL_SRInfo\projeto\contratos'
-nome_arquivo = 'contratos'

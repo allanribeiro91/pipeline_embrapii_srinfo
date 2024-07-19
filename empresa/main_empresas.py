f6c319a576b0b7b7b778535e70b84547097155f3
milena-sgoncalves <milena.goncalves@embrapii.org.br>
2024-07-19 13:51:29 -0300

    Criação do script main_empresas conforme os scripts de projetos e unidades

---
+++ empresa/main_empresas.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+#sys.path
+current_dir = os.path.dirname(os.path.abspath(__file__))
+scripts_public_path = os.path.abspath(os.path.join(current_dir, '..', '..', 'etl_srinfo\\scripts_public'))
+sys.path.append(scripts_public_path)
+
+from scripts_public import baixar_e_juntar_arquivos
+
+def main_empresas():
+    link = 'https://srinfo.embrapii.org.br/company/list/'
+    nome_arquivo = 'empresas'
+    baixar_e_juntar_arquivos(link, current_dir, nome_arquivo)
+
+
+if __name__ == "__main__":
+    main_empresas()

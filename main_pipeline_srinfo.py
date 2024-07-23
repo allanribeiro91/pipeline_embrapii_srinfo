f7e4f97c740c5cfb5b4f516d376e996924fcb6fb
milena-sgoncalves <milena.goncalves@embrapii.org.br>
2024-07-23 12:25:49 -0300

    modificações nos scripst main estudantes, macroentregas, pedidos_pi e pipeline

--- main_pipeline_srinfo.py
+++ main_pipeline_srinfo.py
@@ -8,10 +8,16 @@ sys.path.append(project_root)
 # Importar o módulo principal de contratos
 from projeto.contratos.main_contratos import main_contratos
 from projeto.projetos.main_projetos import main_projetos
+from projeto.estudantes.main_estudantes import main_estudantes
+from projeto.pedidos_pi.main_pedidos_pi import main_pedidos_pi
+from projeto.macroentregas.main_macroentregas import main_macroentregas
 
 def main_pipeline_srinfo():
     main_contratos()
     main_projetos()
+    main_estudantes()
+    main_pedidos_pi()
+    main_macroentregas()
 
 if __name__ == "__main__":
     main_pipeline_srinfo()
--- projeto/estudantes/main_estudantes.py
+++ projeto/estudantes/main_estudantes.py
@@ -1,20 +1,31 @@
 import os
 import sys
+from dotenv import load_dotenv
 
-#sys.path
-current_dir = os.path.dirname(os.path.abspath(__file__))
-scripts_public_path = os.path.abspath(os.path.join(current_dir, '..', '..', '..', 'etl_srinfo\scripts_public'))
-sys.path.append(scripts_public_path)
-from scripts_public import baixar_e_juntar_arquivos
+#carregar .env
+load_dotenv()
+ROOT = os.getenv('ROOT')
 
-scripts_estudantes_path = os.path.abspath(os.path.join(current_dir, 'scripts'))
-sys.path.append(scripts_estudantes_path)
+#Definição dos caminhos
+PATH_ROOT = os.path.abspath(os.path.join(ROOT))
+SCRIPTS_PUBLIC_PATH = os.path.abspath(os.path.join(ROOT, 'scripts_public'))
+CURRENT_DIR = os.path.abspath(os.path.join(ROOT, 'projeto', 'estudantes'))
+SCRIPTS_PATH = os.path.abspath(os.path.join(CURRENT_DIR, 'scripts'))
+
+#Adicionar caminhos ao sys.path
+sys.path.append(PATH_ROOT)
+sys.path.append(SCRIPTS_PUBLIC_PATH)
+sys.path.append(SCRIPTS_PATH)
+
+#Importar módulos necessários
+from scripts_public.scripts_public import baixar_e_juntar_arquivos
 from tratamento_dados import processar_dados
 
+#Definição da função
 def main_estudantes():
     link = 'https://srinfo.embrapii.org.br/projects/student/list'
     nome_arquivo = 'estudantes'
-    baixar_e_juntar_arquivos(link, current_dir, nome_arquivo)
+    baixar_e_juntar_arquivos(link, CURRENT_DIR, nome_arquivo)
     processar_dados()
     print('main_estudantes finalizado!!')
 

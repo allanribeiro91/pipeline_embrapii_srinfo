aee4d2f4245179a2a45abf11e59dcaa3699810ac
milena-sgoncalves <milena.goncalves@embrapii.org.br>
2024-07-23 12:36:08 -0300

    Modificação rota main_info_empresas

--- empresa/info_empresas/main_info_empresas.py
+++ empresa/info_empresas/main_info_empresas.py
@@ -1,17 +1,29 @@
 import os
 import sys
+from dotenv import load_dotenv
 
-#sys.path
-current_dir = os.path.dirname(os.path.abspath(__file__))
-scripts_public_path = os.path.abspath(os.path.join(current_dir, '..', '..', '..', 'etl_srinfo\scripts_public'))
-sys.path.append(scripts_public_path)
+#carregar .env
+load_dotenv()
+ROOT = os.getenv('ROOT')
 
-from scripts_public import baixar_e_juntar_arquivos
+#Definição dos caminhos
+PATH_ROOT = os.path.abspath(os.path.join(ROOT))
+SCRIPTS_PUBLIC_PATH = os.path.abspath(os.path.join(ROOT, 'scripts_public'))
+CURRENT_DIR = os.path.abspath(os.path.join(ROOT, 'empresa', 'info_empresas'))
+SCRIPTS_PATH = os.path.abspath(os.path.join(CURRENT_DIR, 'scripts'))
+
+#Adicionar caminhos ao sys.path
+sys.path.append(PATH_ROOT)
+sys.path.append(SCRIPTS_PUBLIC_PATH)
+sys.path.append(SCRIPTS_PATH)
+
+#Importar módulos necessários
+from scripts_public.scripts_public import baixar_e_juntar_arquivos
 
 def main_info_empresas():
     link = 'https://srinfo.embrapii.org.br/company/list/'
     nome_arquivo = 'info_empresas'
-    baixar_e_juntar_arquivos(link, current_dir, nome_arquivo)
+    baixar_e_juntar_arquivos(link, CURRENT_DIR, nome_arquivo)
 
 
 if __name__ == "__main__":

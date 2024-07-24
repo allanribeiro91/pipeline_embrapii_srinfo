403721d0dc24bc5459b8cca1282131ea1b39fe4f
allanribeiro91 <akosr91@gmail.com>
2024-07-24 11:59:09 -0300

    ajustes no main_estudante.

--- projeto/estudantes/main_estudantes.py
+++ projeto/estudantes/main_estudantes.py
@@ -21,7 +21,8 @@ sys.path.append(CURRENT_DIR)
 #Importar módulos necessários
 from scripts_public.scripts_public import baixar_e_juntar_arquivos
 from scripts_public.copiar_arquivos_finalizados_para_dwpii import copiar_arquivos_finalizados_para_dwpii
-from scripts.tratamento_dados import processar_dados
+from scripts_public.processar_excel import processar_excel
+# from scripts.tratamento_dados import processar_dados
 
 #Definição da função
 def main_estudantes(driver):
@@ -30,6 +31,56 @@ def main_estudantes(driver):
     baixar_e_juntar_arquivos(driver, link, CURRENT_DIR, nome_arquivo)
     processar_dados()
     copiar_arquivos_finalizados_para_dwpii(DIRETORIO_ARQUIVOS_FINALIZADOS)
+
+
+
+# Definições dos caminhos e nomes de arquivos
+origem = os.path.join(ROOT, 'projeto', 'estudantes', 'step_2_stage_area')
+destino = os.path.join(ROOT, 'projeto', 'estudantes', 'step_3_data_processed')
+nome_arquivo = "estudantes.xlsx"
+arquivo_origem = os.path.join(origem, nome_arquivo)
+arquivo_destino = os.path.join(destino, nome_arquivo)
+
+# Campos de interesse e novos nomes das colunas
+campos_interesse = [
+    "Código do projeto / Projeto Espelho",
+    "Unidade EMBRAPII",
+    "CPF",
+    "Nome",
+    "Titulação",
+    "Formação Acadêmica",
+    "Nível de formação (Em andamento)",
+    "Atividade / Função",
+    "Disponibilidade (horas/mês)",
+    "Data de início das atividades no projeto",
+    "Data de término das atividades no projeto",
+    "Possui Bolsa",
+    "Observações ou comentários",
+]
+
+novos_nomes_e_ordem = {
+    "Código do projeto / Projeto Espelho": "codigo_projeto",
+    "Unidade EMBRAPII": "unidade_embrapii",
+    "CPF": "cpf",
+    "Nome": "nome",
+    "Titulação": "titulacao",
+    "Formação Acadêmica": "formacao",
+    "Nível de formação (Em andamento)": "nivel_formacao_cursando",
+    "Atividade / Função": "atividade",
+    "Disponibilidade (horas/mês)": "disponibilidade",
+    "Data de início das atividades no projeto": "data_inicio_atividades",
+    "Data de término das atividades no projeto": "data_termino_atividades",
+    "Possui Bolsa": "bolsista",
+    "Observações ou comentários": "observacoes",
+}
+
+# Campos de data e valor
+campos_data = ['data_inicio_atividades', 'data_termino_atividades']
+
+def processar_dados():
+    print('Processou dados')
+    processar_excel(arquivo_origem, campos_interesse, novos_nomes_e_ordem, arquivo_destino, campos_data)
+
 
 
 #Executar função
